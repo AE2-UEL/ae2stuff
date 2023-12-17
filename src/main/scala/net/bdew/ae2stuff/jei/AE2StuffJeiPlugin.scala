12f99c91a338c777ef078ef6b1ccbb63498c0d29
serenibyss <10861407+serenibyss@users.noreply.github.com>
2023-12-16 22:37:43 -0600

    Fix adv inscriber not being a JEI catalyst

--- src/main/scala/net/bdew/ae2stuff/jei/AE2StuffJeiPlugin.scala
+++ src/main/scala/net/bdew/ae2stuff/jei/AE2StuffJeiPlugin.scala
@@ -28,10 +28,14 @@ package net.bdew.ae2stuff.jei
 
 import mezz.jei.api.recipe.VanillaRecipeCategoryUid
 import mezz.jei.api.{IModPlugin, IModRegistry, JEIPlugin}
+import net.bdew.ae2stuff.machines.inscriber.BlockInscriber
+import net.minecraft.item.ItemStack
 
 @JEIPlugin
 class AE2StuffJeiPlugin extends IModPlugin {
   override def register(registry: IModRegistry): Unit = {
     registry.getRecipeTransferRegistry.addRecipeTransferHandler(EncoderTransferHandler, VanillaRecipeCategoryUid.CRAFTING)
+
+    registry.addRecipeCatalyst(new ItemStack(BlockInscriber), "appliedenergistics2.inscriber")
   }
 }
